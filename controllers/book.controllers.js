309abbfa15376e3f59692b597013a00084d1c02f
ahmadhamzh <ahmadhamzh@ymail.com>
2021-09-13 17:52:45 +0300

    lab 12

--- controllers/book.controllers.js
+++ controllers/book.controllers.js
@@ -1,19 +1,53 @@
 'use strict';
 
-const bookModel=require('../models/book.model');
+const { request, response } = require('express');
+// const { request, response } = require('express');
+const bookModel = require('../models/book.model');
 
 const getBooks = (request, response) => {
 
-    bookModel.find((error, booksData) => {
-        if(error){
+  bookModel.find((error, booksData) => {
+    if (error) {
       response.send(error);
-        }
-        response.json(booksData)
-    });
+    }
+    response.json(booksData)
+  });
+
+};
+const creatBooks = (request, response) => {
+try {  
+  const { title, description, status, email } = request.body;  
+  const newBook = new bookModel({
+    title, description, status, email
+  })
+  newBook.save()
+  response.json(newBook)
+} catch (error) {
   
-  };
+  response.send(error)
+}
+}
+
+const deleteBook = (request, response) => {
   
-  
-  module.exports = {
-    getBooks
-  }
+    
+    const bookId = request.params.book_id
+    bookModel.deleteOne({_id: bookId}, (error, deletedData)=>{
+      if (error) {
+        
+        response.send(error)
+      }
+      response.json(deletedData)
+      console.log(request.params)
+    })
+    
+ 
+
+}
+
+module.exports = {
+  getBooks,
+  creatBooks,
+  deleteBook
+
+}
